1dc1b3045dae4d7175aac8fa4bd9686cd5e702fa
Christian <christianperez.rbb@gmail.com>
2024-04-01 00:01:59 +0200

    feat: create single user logical.

--- src/app/Pages/single-user/single-user.component.ts
+++ src/app/Pages/single-user/single-user.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-single-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './single-user.component.html',
   styleUrl: './single-user.component.css'
 })
 export class SingleUserComponent {
+    http = inject(HttpClient);
+    id:any;
+    client: any = [];
+  
+    constructor() {
+      this.id = "";
+    }
+
+    userDetail() {
+
+      const url = `https://reqres.in/api/users/${this.id}`;
+
+      this.http.get(url).subscribe((response: any) => {
+        this.client = response.data;
+        console.log(this.client);
+      });
+    }
 
 }
