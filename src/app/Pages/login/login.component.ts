4d9c17738d186f3405a5500d44c67021ccdfae06
Christian <christianperez.rbb@gmail.com>
2024-03-31 23:53:28 +0200

    feat: create login logical.

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  http = inject(HttpClient);
+  email: string;
+  password: string;
 
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
+
+  login() {
+    const data = {
+      email: this.email,
+      password: this.password,
+    };
+    const url = 'https://reqres.in/api/login';
+
+    this.http.post(url, data).subscribe((response: any) => {
+      console.log(response);
+    });
+    console.log(
+      `Registrando con Email: ${this.email}, Password: ${this.password}`
+    );
+  }
 }
