9273fd187cf6036d47a462034a6d6665109d74b2
Christian <christianperez.rbb@gmail.com>
2024-04-01 00:17:12 +0200

    update: update routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './Pages/users/user.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { SingleUserComponent } from './Pages/single-user/single-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'prueba/users', component: UserComponent },
+  { path: 'prueba/login', component: LoginComponent },
+  { path: 'prueba/single-user', component: SingleUserComponent },
+];
