90d9b59f8a0770ff75ac6de0ac95a075997114c2
Christian <christianperez.rbb@gmail.com>
2024-04-01 00:20:10 +0200

    update: modified logical of login.

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginComponent, FormsModule],
+  imports: [LoginComponent, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -13,10 +14,16 @@ export class LoginComponent {
   http = inject(HttpClient);
   email: string;
   password: string;
+  token: any;
+  logedIn: boolean;
+  error:boolean;
+
 
   constructor() {
     this.email = '';
     this.password = '';
+    this.logedIn = false;
+    this.error=false;
   }
 
   login() {
@@ -26,11 +33,17 @@ export class LoginComponent {
     };
     const url = 'https://reqres.in/api/login';
 
+    if (this.email === '' || this.password === '') {
+      console.log('Email o Password vacios');
+      this.error=true;
+      return;
+    }
+
     this.http.post(url, data).subscribe((response: any) => {
       console.log(response);
+      this.token = response.token;
+      this.logedIn = true;
     });
-    console.log(
-      `Registrando con Email: ${this.email}, Password: ${this.password}`
-    );
+    console.log(`Logeado con Email: ${this.email}, Password: ${this.password}`);
   }
 }
